4fbc0c2dfe62da04605284bc9ddd37b49abfef61
John Lilly <john.lilly@gmail.com>
2024-12-16 08:10:09 -0800

    Initial site setup fixed

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,30 @@
-<antArtifact identifier="eleventy-basic-config" type="application/vnd.ant.code" language="javascript" title="Basic 11ty Configuration">
 module.exports = function(eleventyConfig) {
-  // Copy assets directly
+  // Copy assets directly to the output
   eleventyConfig.addPassthroughCopy('src/assets');
 
+  // Create a collection for blog posts
+  eleventyConfig.addCollection('posts', function(collection) {
+    return collection.getFilteredByGlob('src/posts/*.md')
+      .sort((a, b) => new Date(b.data.date) - new Date(a.data.date));
+  });
+
+  // Add some useful filters
+  // Format dates
+  eleventyConfig.addFilter('dateFormat', function(date) {
+    return new Date(date).toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    });
+  });
+
+  // Return an excerpt from a blog post
+  eleventyConfig.addFilter('excerpt', function(post) {
+    const content = post.content;
+    return content.substring(0, 250) + '...';
+  });
+
+  // Configuration options
   return {
     dir: {
       input: 'src',
@@ -11,6 +33,7 @@ module.exports = function(eleventyConfig) {
       data: '_data'
     },
     templateFormats: ['md', 'njk', 'html'],
-    markdownTemplateEngine: 'njk'
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk'
   };
 };
